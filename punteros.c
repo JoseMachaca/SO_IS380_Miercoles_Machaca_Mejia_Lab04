61be7e1ddb465be253253c8dd607a6c459f4a6eb
josedev <josemachacamejia@gmail.com>
2026-09-10 14:32:21 -0500

    Implementa la funcion main con las pruebas comparativas de intercambio

--- punteros.c
+++ punteros.c
@@ -12,3 +12,20 @@ int temp = *a;
 *a = *b;
 *b = temp;
 }
+
+int main() {
+
+int x = 5, y = 10;
+
+printf("Antes: __x=%d_y=%d\n", x, y);
+
+intercambiarValor(x, y);
+printf("Sin_puntero: Lux=%d_y=%d (NO1cambio)\n", x, y);
+
+intercambiar Puntero (&x, &y);
+printf("Con puntero:x=%d_y=%d(SI1cambio)\n", x, y);
+
+return 0;
+
+}
+
